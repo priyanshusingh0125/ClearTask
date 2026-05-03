06d63813b1f61e6ea0795940e477f93867470334
Priyanshu Singh <priyanshusingh@Priyanshus-MacBook-Air.local>
2026-05-03 22:54:04 +0530

    fix fs error

--- server/server.js
+++ server/server.js
@@ -54,8 +54,8 @@ app.get("/", (req, res) => {
   res.send("Priyanhu's Backend is running");
 });
 
-// server start
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
-});
+const PORT = process.env.PORT || 5001;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
